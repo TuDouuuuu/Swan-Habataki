cf77cd238c08131ed71ce75238d020c0bf0453ce
TuDouuuuu <932087357@qq.com>
2019-11-03 22:48:21 +0800

    更新了莫队算法。

--- 莫队算法/区间查询，统计两个相同概率.cpp
+++ 莫队算法/区间查询，统计两个相同概率.cpp
@@ -1,3 +1,8 @@
+/*
+    HYSBZ - 2038 小Z的袜子
+    统计[L,R]区间内选两只袜子，颜色相同的概率。区间查询，统计两个相同概率。
+    N,M ≤ 50000，1 ≤ L < R ≤ N，Ci ≤ N。
+*/
 #define ll long long
 const ll SIZE=50050;
 struct node{//查询
@@ -5,9 +10,10 @@ struct node{//查询
     ll id,res1,res2;
 }q[SIZE];
 
-ll c[SIZE];
-ll n,m,unit;
+ll c[SIZE];//n只袜子的颜色
+ll n,m,unit;//n只瓦子，m次查询
 ll num[SIZE];
+
 ll com(ll nn,ll mm){
     if(mm>nn/2)mm=nn-mm;
     ll aa=1,bb=1;
@@ -23,17 +29,20 @@ ll com(ll nn,ll mm){
     return tmp;
 }
 
+//朴素排序
 bool cmp(node a,node b){
     if(a.L/unit != b.L/unit)return a.L/unit < b.L/unit;
     else return a.R < b.R;
 }
 
+//对答案进行排序
 bool cmp_id(node a,node b){
     return a.id<b.id;
 }
 
 int main(){
-    cin>>n>>m;unit=sqrt(n);
+    cin>>n>>m;
+    unit=sqrt(n);//分块
     for(ll i=1;i<=n;i++)cin>>c[i];
     for(ll i=1;i<=m;i++){
         q[i].id=i;
@@ -71,7 +80,4 @@ int main(){
         ll gcdd=__gcd(sum,under);
         q[i].res1=sum/gcdd;q[i].res2=under/gcdd;
     }
-    sort(q+1,q+1+m,cmp_id);
-    for(ll i=1;i<=m;i++)
-        cout<<q[i].res1<<"/"<<q[i].res2<<endl;
 }
