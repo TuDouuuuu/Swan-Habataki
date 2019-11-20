9e7cf7280ed57862140c58ca50942c98eabd73fa
TuDouuuuu <932087357@qq.com>
2019-11-20 21:09:39 +0800

    icpc沈阳

--- 数论/GCD/一些性质.cpp
+++ 数论/GCD/一些性质.cpp
@@ -4,7 +4,7 @@
 
 //gcd(x^a-1,x^b-1)=x^gcd(a,b)-1
 
-//GCD(a^m?b^m,a^n?b^n) = a^GCD(m,n) ?b^GCD(m,n) (a,b 互质)
+//GCD(a^m-b^m,a^n-b^n) = a^GCD(m,n) - b^GCD(m,n) (a,b 互质)
 
 //gcd（b*t+a，b）=gcd（a，b）
 //所以和b有关的gcd以phi(b)为循环节循环
