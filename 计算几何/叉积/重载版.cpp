c0d4a29b2afc823f65ac9641a5dfac3e208630bb
shk314159 <296155457@qq.com>
2020-07-30 09:32:41 +0800

    旋转坐标系更新板子

--- 计算几何/叉积/重载版.cpp
+++ 计算几何/叉积/重载版.cpp
@@ -1,26 +1,24 @@
-struct point
-{
+struct point {
     double x, y;
     point(double x = 0, double y = 0) : x(x), y(y) {}
-    point operator+(const point &t) const
-    {
+    point operator+(const point &t) const {
         return point(x + t.x, y + t.y);
     } //a + b
-    point operator-(const point &t) const
-    {
+    point operator-(const point &t) const {
         return point(x - t.x, y - t.y);
     } //a - b
-    double operator*(const point &t) const
-    {
+    double operator*(const point &t) const {
         return x * t.x + y * t.y;
     } //a * b
-    double operator^(const point &t) const
-    {
+    double operator^(const point &t) const {
         return x * t.y - y * t.x;
     } //a X b
-    double dis(const point &t)
-    {
+    double dis(const point &t) {
         return sqrt((x - t.x) * (x - t.x) + 
 					(y - t.y) * (y - t.y));
     }
+	point eotate(point const &o, double angle) {//绕p点逆时针转angle角度 
+		return point(o.x + (x - o.x) * cos(angle) - (y - o.y) * sin(angle),
+					o.y + (x - o.x) * sin(angle) + (y - o.y) * cos(angle));
+	}
 };
