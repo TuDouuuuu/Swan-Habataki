7c40070bfddc214ba7de2fa3993b53efe1522528
TuDouuuuu <932087357@qq.com>
2020-08-07 14:35:31 +0800

    add SAM template.

---
+++ 字符串/后缀自动机SAM.cpp
@@ -0,0 +1,43 @@
+class Suffix_Automaton {
+public:
+    int rt, link[MAXN], maxlen[MAXN], trans[MAXN][MAXC];
+
+    void init() {
+        rt = 1;
+        link[1] = maxlen[1] = 0;
+        memset(trans[0], 0, sizeof(trans[0]));
+    }
+
+    Suffix_Automaton() { init(); }
+
+    inline int insert(int ch, int last) {   // main: last = 1
+        if (trans[last][ch]) {
+            int p = last, x = trans[p][ch];
+            if (maxlen[p] + 1 == maxlen[x]) return x;
+            else {
+                int y = ++rt;
+                maxlen[y] = maxlen[p] + 1;
+                for (int i = 0; i < MAXC; i++) trans[y][i] = trans[x][i];
+                while (p && trans[p][ch] == x) trans[p][ch] = y, p = link[p];
+                link[y] = link[x], link[x] = y;
+                return y;
+            }
+        }
+        int z = ++rt, p = last;
+        maxlen[z] = maxlen[last] + 1;
+        while (p && !trans[p][ch]) trans[p][ch] = z, p = link[p];
+        if (!p) link[z] = 1;
+        else {
+            int x = trans[p][ch];
+            if (maxlen[p] + 1 == maxlen[x]) link[z] = x;
+            else {
+                int y = ++rt; 
+                maxlen[y] = maxlen[p] + 1;
+                for (int i = 0; i < MAXC; i++) trans[y][i] = trans[x][i];
+                while (p && trans[p][ch] == x) trans[p][ch] = y, p = link[p];
+                link[y] = link[x], link[z] = link[x] = y;
+            }
+        }
+        return z;
+    }
+} sa;
